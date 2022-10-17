4e191c75fe0aa0e3fe7400d4fa0bdf8ed86b180d
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2022-10-17 18:34:35 +0200

    dasdas

--- UD3/02_custom_filter/main.js
+++ UD3/02_custom_filter/main.js
@@ -1,9 +1,37 @@
 const assert = require('assert').strict;
 
 function select(array, condition) {
+    let result = []
+
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+
+        if (condition(element)) {
+            result.push(element)
+
+        }
+    }
+    return console.log(result)
 }
 
 let values = [1, 2, 3, 5, 7, 13, 17, 23, 29]
+
+
+
+function pairs(values) {
+    return values % 2 === 0
+}
+
+
+function gt15(values) {
+    return values > 15
+}
+
+function lt10(values) {
+    return values < 10
+}
+
+
 
 // sólo pares
 assert.deepStrictEqual(select(values, pairs), [2])
